dd647e0563aca698a225d49e5fb2a3ea513abeaf
Edvin Hultberg <hultberg@mittnett.net>
2017-06-24 15:05:22 +0200

    fix(entityexplode): deny only creeper from exploding

--- src/main/java/net/mittnett/reke/Rekeverden/listeners/EntityListener.java
+++ src/main/java/net/mittnett/reke/Rekeverden/listeners/EntityListener.java
@@ -1,17 +1,20 @@
-/*    */ package net.mittnett.reke.Rekeverden.listeners;
-/*    */ 
-/*    */ import org.bukkit.event.EventPriority;
-/*    */ import org.bukkit.event.Listener;
-/*    */ import org.bukkit.event.entity.EntityExplodeEvent;
-/*    */ 
-/*    */ public class EntityListener implements Listener
-/*    */ {
-/*    */   @org.bukkit.event.EventHandler(priority=EventPriority.NORMAL, ignoreCancelled=true)
-/*    */   public void onEntityExplode(EntityExplodeEvent event)
-/*    */   {
-/* 12 */     event.setCancelled(true);
-/*    */   }
-/*    */ }
+package net.mittnett.reke.Rekeverden.listeners;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+
+public class EntityListener implements Listener {
+
+  @org.bukkit.event.EventHandler(priority = EventPriority.NORMAL, ignoreCancelled = true)
+  public void onEntityExplode(EntityExplodeEvent event) {
+    if (event.getEntityType() == EntityType.CREEPER) {
+      event.setCancelled(true);
+    }
+  }
+
+}
 
 
 /* Location:              /Users/edvin/Rekeverden-0.0.1-SNAPSHOT.jar!/net/mittnett/reke/Rekeverden/listeners/EntityListener.class
